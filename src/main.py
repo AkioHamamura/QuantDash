89bd02c6ced5ce3c004bac17ea956865e25e6e88
Jakob Ildstad <jakobildstad@gmail.com>
2025-07-22 21:11:02 +0200

    feat: caching of stock data. 10x faster

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
 # Main entry point for QuantDash application
 from data.data_fetcher import fetch_stock_data
+import matplotlib.pyplot as plt
 
 def main():
     #from src.dashboard.app import run_dash_app
 
     ## Run the Dash application
     #run_dash_app()
-    data = fetch_stock_data("AAPL", start_date="2020-01-01", end_date="2025-01-01")
-    print(data.head()) # type: ignore
-
-
-
-
-
-
+    data = fetch_stock_data("KBGGY")
+    if data is not None:
+        plt.plot(data.index, data['Close'])
+        plt.title("KBGGY Stock Price")
+        plt.xlabel("Date")
+        plt.ylabel("Close Price")
+        plt.show()
 
 
 
